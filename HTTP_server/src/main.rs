466c54e7d635a7f38b63a1a10637bc63b7435d96
takao-h <takao.hayashi@zozo.com>
2023-01-16 22:34:48 +0900

    Fix

--- HTTP_server/src/main.rs
+++ HTTP_server/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,7 +14,7 @@ fn main() {
 
 fn handle_connection(mut _stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    _stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
 
@@ -25,8 +26,8 @@ fn handle_connection(mut _stream: TcpStream) {
 
         let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        _stream.write(response.as_bytes()).unwrap();
+        _stream.flush().unwrap();
     } else {
         let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
         let mut file = File::open("404.html").unwrap();
@@ -36,7 +37,7 @@ fn handle_connection(mut _stream: TcpStream) {
 
         let response = format!("{}{}", status_line, contents);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        _stream.write(response.as_bytes()).unwrap();
+        _stream.flush().unwrap();
     }
 }

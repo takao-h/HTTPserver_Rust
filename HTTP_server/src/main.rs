d394269019d8ad4d06874d5c0a80249c1bf4d4a1
HAYASHI TAKAO <rem@macbook-3.local>
2023-01-21 20:26:47 +0900

    ADD sleep that ready for malti thread

--- HTTP_server/src/main.rs
+++ HTTP_server/src/main.rs
@@ -4,6 +4,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
 use std::stream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,11 +21,15 @@ fn handle_connection(mut _stream: TcpStream) {
     _stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    let put: &[u8; 16] = b"PUT / HTTP/1.1\r\n";
-    let post: &[u8; 17] = b"POST / HTTP/1.1\r\n";
-    let delete: &[u8; 19] = b"DELETE / HTTP/1.1\r\n";
+    // let put: &[u8; 16] = b"PUT / HTTP/1.1\r\n";
+    // let post: &[u8; 17] = b"POST / HTTP/1.1\r\n";
+    // let delete: &[u8; 19] = b"DELETE / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")

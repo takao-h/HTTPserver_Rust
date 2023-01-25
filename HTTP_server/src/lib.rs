0b475352bcd832eec5ff9657b2571cab43c0482f
takao-h <takao.hayashi@zozo.com>
2023-01-25 23:28:44 +0900

    Add excute method

--- HTTP_server/src/lib.rs
+++ HTTP_server/src/lib.rs
@@ -1,7 +1,11 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+    // --snip--
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+
     }
 }

b9877bea5a84e8b38707beb82a97b8c1e48ff185
takao-h <takao.hayashi@zozo.com>
2023-01-26 23:35:43 +0900

    Add worker

--- HTTP_server/src/lib.rs
+++ HTTP_server/src/lib.rs
@@ -1,30 +1,39 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
-    /// 新しいThreadPoolを生成する。
-    ///
-    /// sizeがプールのスレッド数です。
-    ///
-    /// # パニック
-    ///
-    /// sizeが0なら、`new`関数はパニックします。
-    ///
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///　
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
+    // --snip--
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool {
+            workers
+        }
     }
     // --snip--
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
     }
 }

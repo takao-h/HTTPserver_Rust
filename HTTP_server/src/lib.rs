e84b9deecdd546c975608af037afb38e7ef1b4de
takao-h <takao.hayashi@zozo.com>
2023-01-26 23:28:51 +0900

    Add new function

--- HTTP_server/src/lib.rs
+++ HTTP_server/src/lib.rs
@@ -1,11 +1,30 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// 新しいThreadPoolを生成する。
+    ///
+    /// sizeがプールのスレッド数です。
+    ///
+    /// # パニック
+    ///
+    /// sizeが0なら、`new`関数はパニックします。
+    ///
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///　
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        ThreadPool
+    }
     // --snip--
     pub fn execute<F>(&self, f: F)
-        where
-            F: FnOnce() + Send + 'static
+    where
+        F: FnOnce() + Send + 'static,
     {
-
     }
 }

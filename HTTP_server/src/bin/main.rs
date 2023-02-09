07336402cd0f5b894d2642adc5d5875a15c45c12
takao-h <takao.hayashi@zozo.com>
2023-02-09 23:03:18 +0900

    Fix

--- HTTP_server/src/bin/main.rs
+++ HTTP_server/src/bin/main.rs
@@ -1,5 +1,4 @@
 extern crate HTTP_server;
-use HTTP_server::ThreadPool;
 use std::fmt::format;
 use std::fs::File;
 use std::io::prelude::*;
@@ -7,18 +6,21 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+use HTTP_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut _stream: TcpStream) {
